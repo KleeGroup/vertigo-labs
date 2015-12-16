771e3c105873e6beddc705937b12699a709176b1
phlppchrtn <pchretien55@gmail.com>
2015-12-16 18:47:31 +0100

    [folio] moved to vertigo-folio

--- vertigo-folio-impl/src/main/java/io/vertigo/folio/impl/enhancement/EnhancementPlugin.java
+++
@@ -1,9 +0,0 @@
-package io.vertigo.folio.impl.enhancement;
-
-import io.vertigo.folio.document.model.Document;
-import io.vertigo.folio.metadata.MetaDataContainer;
-import io.vertigo.lang.Plugin;
-
-public interface EnhancementPlugin extends Plugin {
-	MetaDataContainer extract(final Document document) throws Exception;
-}
--- vertigo-folio-impl/src/main/java/io/vertigo/folio/impl/namedentity/TokenizerPlugin.java
+++
@@ -1,9 +0,0 @@
-package io.vertigo.folio.impl.namedentity;
-
-import io.vertigo.lang.Plugin;
-
-import java.util.List;
-
-public interface TokenizerPlugin extends Plugin {
-	List<String> tokenize(final String text);
-}
