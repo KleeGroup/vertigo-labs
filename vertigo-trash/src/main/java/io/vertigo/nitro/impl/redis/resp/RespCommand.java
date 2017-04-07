f3778eb934a8be4e8c6df970b056f946803f3fe9
phlppchrtn <pchretien55@gmail.com>
2017-04-07 19:43:11 +0200

    +simple

--- vertigo-trash/src/main/java/io/vertigo/nitro/impl/redis/resp/RespCommand.java
+++ vertigo-trash/src/main/java/io/vertigo/nitro/impl/redis/resp/RespCommand.java
@@ -1,24 +1,21 @@
 package io.vertigo.nitro.impl.redis.resp;
 
+import java.util.Arrays;
+
+import io.vertigo.lang.Assertion;
+
 public final class RespCommand {
 	private final String name;
 	private final String[] args;
 
 	RespCommand(final String name, final String... args) {
-		if (name == null) {
-			throw new NullPointerException("name is required");
-		}
-		if (args == null) {
-			throw new NullPointerException("args is required, may be empty");
-		}
-		for (final String item : args) {
-			if (item == null) {
-				throw new NullPointerException("arg can not be null");
-			}
-		}
+		Assertion.checkNotNull(name, "name is required");
+		Assertion.checkNotNull(args, "args is required, may be empty");
+		Arrays.stream(args).forEach(arg -> Assertion.checkNotNull(arg, "arg can not be null"));
 		//-------------------------------------------------
 		this.name = name.toLowerCase();
 		this.args = args;
+
 	}
 
 	public String getName() {
